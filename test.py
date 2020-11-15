f5c18cb70cbe8cd5e586fa22743ac6a6d42db556
Zachary Picker <zpic0094@sydney.edu.au>
2020-11-15 14:24:02 +1100

    changed param in test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from effects.spectrogram import BasicSpectrogram, RainbowSpectrogram
 
 def run():
     spectrogram = BasicSpectrogram(x_lim=1000, y_lim = 1000, line_args = {'c': 'orange'})
-    rainbow = RainbowSpectrogram(n_lines=100)
+    rainbow = RainbowSpectrogram(n_lines=50)
     
     effects = [rainbow, spectrogram]
     P = Performance(effects, blit=True)
